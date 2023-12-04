f526047bf69371ee947cd58547cceb16c8944d22
GeraldiAxel <geraldi.axel@gmail.com>
2023-12-04 14:05:56 +0700

    comment bookmark model
    bookmark is only a pivot table, doesn;t need a model

--- app/Models/Bookmark.php
+++ app/Models/Bookmark.php
@@ -9,11 +9,11 @@ class Bookmark extends Model
 {
     use HasFactory;
 
-    public function user(){
-        return $this->belongsTo(User::class);
-    }
+    // public function user(){
+    //     return $this->belongsTo(User::class);
+    // }
 
-    public function post(){
-        return $this->belongsTo(Post::class);
-    }
+    // public function post(){
+    //     return $this->belongsTo(Post::class);
+    // }
 }
