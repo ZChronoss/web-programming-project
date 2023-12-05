0dfaa363ef45d6f6aa27b476587190bee43d450b
Nicholas Declan <declanhtp@gmail.com>
2023-12-05 21:35:19 +0700

    Fixed create post page

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -10,20 +10,10 @@
                         Create Post
                     </div>
                     <hr class="m-0"> <!-- Line -->
-                    <div class="card-body p-3">
+                    <div class="card-body p-0">
                         <form method="POST" enctype="multipart/form-data" action="/post/store">
                             @csrf
-                            <div class="row p-0">
-                                <label for="image" class="fw-bold px-3 mb-3">Upload Image</label>
-
-                                <input type="file" class="form-control-file px-3 mb-3" id="image" name="image">
-
-                                @error('image')
-                                    <strong>{{ $message }}</strong>
-                                @enderror
-                            </div>
-
-                            <div class="row p-0 mb-3">
+                            <div class="row p-3">
                                 <label for="caption" class="fw-bold mb-3 px-3">Caption</label>
 
                                 <div class="col px-3">
@@ -37,19 +27,30 @@
                                 </div>
                             </div>
 
-                        </form>
-                    </div>
+                            <div class="row p-3">
+                                <label for="image" class="fw-bold px-3 mb-3">Upload Image</label>
 
-                    <hr class="m-0"> <!-- Line -->
-                    <div class="row p-3">
-                        <div class="col px-3 d-flex justify-content-end">
-                            <button type="submit" class="btn md-3 btn-primary">
-                                Create Post
-                            </button>
-                            {{-- <span>Don’t have an account?
-                                <a href="">Create Account</a>
-                            </span> --}}
-                        </div>
+                                <input type="file" class="form-control-file px-3 mb-3" id="image" name="image">
+
+                                @error('image')
+                                    <strong>{{ $message }}</strong>
+                                @enderror
+                            </div>
+
+                            <hr class="m-0"> <!-- Line -->
+
+                            <div class="row p-3">
+                                <div class="col px-3 d-flex justify-content-end">
+                                    <button type="submit" class="btn md-3 btn-primary">
+                                        Create Post
+                                    </button>
+                                    {{-- <span>Don’t have an account?
+                                        <a href="">Create Account</a>
+                                    </span> --}}
+                                </div>
+                            </div>
+
+                        </form>
                     </div>
                 </div>
             </div>
